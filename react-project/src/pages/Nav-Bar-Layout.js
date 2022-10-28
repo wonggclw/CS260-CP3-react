89a571962e6a5319cd56210eca4100596c896391
wonggclw <gcw@groundworksdesign.com>
2022-10-28 12:46:53 -0600

    working changes

--- react-project/src/pages/Nav-Bar-Layout.js
+++ react-project/src/pages/Nav-Bar-Layout.js
@@ -1,4 +1,32 @@
 import { Outlet, Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap'
+import $ from 'jquery';
+import Popper from 'popper.js';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function BasicExample() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
 
 const NavBarLayout = () => {
   return (
@@ -16,6 +44,8 @@ const NavBarLayout = () => {
           </li>
         </ul>
       </nav>
+
+      <BasicExample />
 
       <Outlet />
     </>
