6c169e68777c18ed316a284a26d0afbcec3ac5d8
Andrea Melgoza <andreamelgoza2300@gmail.com>
2022-11-03 03:03:43 +0000

    text

--- react-project/src/pages/Colors.js
+++ react-project/src/pages/Colors.js
@@ -1,5 +1,66 @@
 const Colors = () => {
-    return <h1>Colors</h1>;
+    return (
+        <div>
+            <p>There are many types of color schemes, each gives a website a 
+                different feel. Here are a few common types:</p>
+                <p>(info found at <a href="https://colorcodes.io/color-schemes-types/">colorcodes.io</a>)</p>
+            <img class = "body-img" src="https://i.pinimg.com/originals/38/5c/66/385c66d86e1e6d450f22d03e14ea5176.png"/>
+            <h3 class="body-head">Complementary</h3>
+                <p>A complementary color scheme is one that uses two colors 
+                opposite of each other on a color wheel. It can also include 
+                relevant tints if more than two colors are needed. A 
+                complementary color scheme offers the most color contrast of any
+                color scheme.</p>
+                
+                <h3 class="body-head">Analogous</h3>
+                <p>Analogous color schemes can be created by pairing one color 
+                with two of the colors next to it on a color wheel. If you need 
+                a five-color scheme to just three, you can add two more colors 
+                to the color wheel. Analogous color schemes are softer and have 
+                less contrast making them an alternative choice to monochromatic
+                schemes. Use an analogous scheme when you want to create an 
+                emotional or aesthetic color scheme such as one that represents 
+                the ocean or the Fall season.</p>
+         
+                <h3 class="body-head">Triadic</h3>
+                <p>Triadic color combinations offer high contrast color schemes 
+                with a consistent tone. Triadic color combinations are made by 
+                using three colors equally spaced around the color wheel. A 
+                Triad color scheme is great for creating high contrast between 
+                colors in a design but in order to create some variation, the 
+                colors should be chosen in different tints so that they do not 
+                sit in line when a circle is drawn around the color wheel.</p>
+        
+          
+                <h3 class="body-head">Split Complementary</h3>
+                <p>Split complementary schemes include one dominant color and 
+                two colors that are adjacent to the complement for a 3-color 
+                palette. This allows for a more nuanced palette while keeping 
+                the high-contrast benefits of complementary colors.</p>
+         
+            
+            
+                <h3 class="body-head">Rectangle</h3>
+                <p>Also known as a tetradic color scheme. A tetradic colors 
+                scheme is in the shape of a rectangle on 
+                the color wheel. The rectangle color offers a subtler approach 
+                to color selection than its square counterpart.</p>
+            
+            
+            
+                <h3 class="body-head">Square</h3>
+                <p>In a square colors scheme or diamond-shaped color scheme, 
+                four colors that are equal to each other on a color wheel are 
+                selected. This scheme creates two sets of complementary colors 
+                that add significant contrast to a design, much more than the 
+                rectangular version.</p>
+        
+        <hr class="extra-space"/>
+        <h1> Contact </h1> <p><strong>Call: 801-376-0000 <br/> Email: logo@gmail.com </strong></p>
+         <p>Pair programmed by Andrea Melgoza and Grace Wong</p>
+            
+        </div>
+        );
   };
   
   export default Colors;
--- react-project/src/pages/Home.js
+++ react-project/src/pages/Home.js
@@ -1,5 +1,14 @@
 const Home = () => {
-    return <h1>Home</h1>;
+    return (
+        <div>
+            <h3 class="body extra-space">Our goal is to make your web design life a little easier. Click below to learn more.</h3>
+            <br></br>
+            <hr></hr>
+            <h1> Contact </h1> 
+            <p> Call: 801-376-0000 <br></br> Email: stylesheets@gmail.com </p>
+            <p>Pair programmed by Andrea Melgoza and Grace Wong</p>
+        </div>
+        );
   };
   
   export default Home;
